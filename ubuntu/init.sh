b9d52bafd23b643a368e49b1da5fe9937671badf
Takao Mizuno <takao.mizuno110@gmail.com>
2018-06-10 04:23:17 +0900

    added command of installing curl

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -35,4 +35,5 @@ sudo apt-add-repository ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get install fish
 chsh -s /usr/bin/fish
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 exec $SHELL -l
