c7b8ba386b7b6686a36b8797113c47021934860a
Takao Mizuno <takao.mizuno110@gmail.com>
2018-12-18 18:58:34 +0900

    added -y option

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -7,7 +7,7 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 echo '#############################'
 echo '# make japanese available'
 echo '#############################'
-sudo apt-get install ibus-mozc
+sudo apt-get install -y ibus-mozc
 killall ibus-daemon
 ibus-daemon -d -x &
 echo 'you can setup japanese available, refering https://qiita.com/pg_naoyuki/items/238f6e5060fb838827f6' 
@@ -16,24 +16,13 @@ echo '#############################'
 echo '# install ansible'
 echo '#############################'
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install -y ansible
 
 echo '#############################'
 echo '# run ansible roles'
 echo '#############################'
 sudo ansible-playbook ansible/run.yml -vvv
 
-
-
-
-
-# final command
-sudo apt-add-repository ppa:fish-shell/release-2
-sudo apt-get update
-sudo apt-get install fish
-chsh -s /usr/bin/fish
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-echo 'fisher edc/bass'|fish 
