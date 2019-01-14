25ea58f6807a67e7a33c400d33d4aa3310e8a29a
root <root@tbsasr-demo.x0zcpwarf0ruzln5gas4041mid.bx.internal.cloudapp.net>
2019-01-14 17:12:03 +0000

    deleted --sk and dealed with docker-ce version to install nvidia-docker

--- ubuntu/nvidia/reinstall_nvidia_driver.sh
+++ ubuntu/nvidia/reinstall_nvidia_driver.sh
@@ -1,6 +1,6 @@
 apt-get --purge remove nvidia-*
 apt-get --purge remove cuda-*
-add-apt-repository --sk ppa:graphics-drivers/ppa
+add-apt-repository ppa:graphics-drivers/ppa
 apt-get update
 apt-cache search 'nvidia-[0-9]+$'$ 
 apt install -y nvidia-410
