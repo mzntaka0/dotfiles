29145dad3df294518559757bf1577d2ad4d0895a
Takao Mizuno <takao.mizuno110@gmail.com>
2019-07-11 18:13:40 +0900

    added terraform shell

---
+++ ubuntu/misc/terraform.sh
@@ -0,0 +1,4 @@
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+unzip terraform_0.11.13_linux_amd64.zip
+mv terraform /usr/local/bin
+sudo rm -rf terraform_0.11.13_linux_amd64.zip
--- ubuntu/misc/wine.sh
+++ ubuntu/misc/wine.sh
@@ -1,8 +1,8 @@
-dpkg --add-architecture i386 
-wget -nc https://dl.winehq.org/wine-builds/Release.key 
-apt-key add Release.key 
-apt-add-repository -y -n https://dl.winehq.org/wine-builds/ubuntu/ 
-rm Release.key 
-apt update 
-apt install wine-stable winetricks 
+dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/Release.key
+apt-key add Release.key
+apt-add-repository -y -n https://dl.winehq.org/wine-builds/ubuntu/
+rm Release.key
+apt update
+apt install wine-stable winetricks
 apt install winehq-stable
