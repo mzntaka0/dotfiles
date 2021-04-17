a900e513009546ec510d677ea2b7d1a62d188eac
Takao Mizuno <takao.mizuno110@gmail.com>
2021-04-17 10:06:02 +0900

    updated nodejs installer

--- ubuntu/misc/nodejs.sh
+++ ubuntu/misc/nodejs.sh
@@ -1,6 +1,8 @@
-sudo apt-get install -y nodejs npm
-sudo npm cache clean
-sudo npm install n -g
-sudo n stable
-sudo ln -sf /usr/local/bin/node /usr/bin/node
-sudo apt-get purge -y nodejs npm
+#sudo apt-get install -y nodejs npm
+#sudo npm cache clean
+#sudo npm install n -g
+#sudo n stable
+#sudo ln -sf /usr/local/bin/node /usr/bin/node
+#sudo apt-get purge -y nodejs npm
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt install -y nodejs
