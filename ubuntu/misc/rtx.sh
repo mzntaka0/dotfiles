a4cc62535ede4e69cbd274619f2c8fc2d2db054b
Takao Mizuno <takao.mizuno110@gmail.com>
2023-05-03 16:38:42 +0900

    added rtx

---
+++ ubuntu/misc/rtx.sh
@@ -0,0 +1,4 @@
+wget -qO - https://rtx.pub/gpg-key.pub | gpg --dearmor | sudo tee /usr/share/keyrings/rtx-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/usr/share/keyrings/rtx-archive-keyring.gpg arch=amd64] https://rtx.pub/deb stable main" | sudo tee /etc/apt/sources.list.d/rtx.list
+sudo apt update
+sudo apt install -y rtx
