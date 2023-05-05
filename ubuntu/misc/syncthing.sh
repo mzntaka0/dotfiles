7269fa0781c2330d6c1bb9421590da0587dcc7c0
Takao Mizuno <takao.mizuno110@gmail.com>
2023-05-05 18:41:12 +0900

    added syncthing

---
+++ ubuntu/misc/syncthing.sh
@@ -0,0 +1,4 @@
+curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+apt update
+apt install syncthing
